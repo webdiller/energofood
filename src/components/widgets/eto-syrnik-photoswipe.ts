90ca1b5a00b346b51ef63d616d56026dce783ea3
webdiller <eugenefromrus@gmail.com>
2026-03-30 21:06:07 +1000

    upd: upd app

--- src/components/widgets/eto-syrnik-photoswipe.ts
+++ src/components/widgets/eto-syrnik-photoswipe.ts
@@ -1,8 +1,7 @@
 import PhotoSwipeLightbox from "photoswipe/lightbox";
 
-// Initializes PhotoSwipe for Eto Syrnik gallery.
+// Initializes PhotoSwipe for Eto Syrnik image gallery.
 const galleryId = "eto-syrnik-gallery";
-
 const el = document.getElementById(galleryId);
 if (el) {
   const lightbox = new PhotoSwipeLightbox({
---
+++ src/types/assets-media.d.ts
@@ -0,0 +1,10 @@
+declare module "*.mov" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.MOV" {
+  const src: string;
+  export default src;
+}
+
